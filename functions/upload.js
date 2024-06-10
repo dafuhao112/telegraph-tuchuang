ac8488aaee919069f4fec4b0473cb17750ff8576
dafuhao112 <153726470+dafuhao112@users.noreply.github.com>
2024-06-10 22:47:02 +0800

    Update upload.js

--- functions/upload.js
+++ functions/upload.js
@@ -1,32 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const multer = require('multer');
-
-const upload = multer({
-    limits: {
-        fileSize: 100 * 1024 * 1024 // 设置上传文件大小限制为100MB
-    }
-});
-
-const app = express();
-app.use(bodyParser.json({ limit: '100mb' }));
-app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
-
-// 替换你的`onRequestPost`函数
-app.post('/upload', upload.single('file'), async (req, res) => {
-    const { env } = req;
-    const file = req.file;
-    const url = new URL(req.url);
-    const apikey = env.ModerateContentApiKey;
-    const ModerateContentUrl = apikey ? `https://api.moderatecontent.com/moderate/?key=${apikey}&` : "";
+export async function onRequestPost(context) {
+    const { request, env } = context;
+    const url = new URL(request.url);
+    const apikey = env.ModerateContentApiKey
+    const ModerateContentUrl = apikey ? `https://api.moderatecontent.com/moderate/?key=${apikey}&` : ""
     const ratingApi = env.RATINGAPI ? `${env.RATINGAPI}?` : ModerateContentUrl;
-    const clientIP = req.headers['x-forwarded-for'] || req.headers['clientIP'];
-    const Referer = req.headers['referer'] || "Referer";
-
+    const clientIP = request.headers.get("x-forwarded-for") || request.headers.get("clientIP");
+    const Referer = request.headers.get('Referer') || "Referer";
     const res_img = await fetch('https://telegra.ph/' + url.pathname + url.search, {
-        method: req.method,
-        headers: req.headers,
-        body: req.body,
+        method: request.method,
+        headers: request.headers,
+        body: request.body,
     });
 
     const options = {
@@ -45,6 +28,7 @@ app.post('/upload', upload.single('file'), async (req, res) => {
     if (!env.IMG) {
         return res_img;
     } else {
+        // const newReq = res_img.clone();
         const responseData = await res_img.json();
         try {
             const rating = ratingApi ? await getRating(ratingApi, responseData[0].src) : { rating: 0 };
@@ -54,9 +38,12 @@ app.post('/upload', upload.single('file'), async (req, res) => {
             await insertImageData(env.IMG, responseData[0].src, Referer, clientIP, 5, formattedDate);
         }
 
-        return res.json(responseData);
+        return Response.json(responseData);
     }
-});
+
+    
+}
+
 
 async function getRating(ratingApi, src) {
     const res = await fetch(`${ratingApi}url=https://telegra.ph${src}`);
@@ -69,8 +56,3 @@ async function insertImageData(env, src, referer, ip, rating, time) {
              VALUES ('${src}', '${referer}', '${ip}', ${rating}, 1, '${time}')`
     ).run();
 }
-
-// 启动服务器
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
